4e3ec022d6d2372214e2fa819bb93bc6e0aecee9
IMAD <kimad7141@gmail.com>
2026-02-28 14:43:09 +0000

    the first one who signed is the admin

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -39,7 +39,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'is_admin' => User::count() === 0
+            'is_global_admin' => User::count() === 0
         ]);
 
         event(new Registered($user));
--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -10,7 +10,7 @@
 
     <!-- Navigation Menu -->
     <nav class="flex-1 space-y-2">
-        @if(auth()->user()->is_admin)
+        @if(auth()->user()->is_global_admin)
         <a href="{{ route('dashboard') }}" class="flex items-center space-x-3 px-4 py-3 {{ request()->routeIs('dashboard') ? 'bg-[#f0f1ff] text-[#5649e7]' : 'text-slate-400' }} rounded-2xl font-bold transition-all">
             <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z"/><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z"/></svg>
             <span>Dashboard</span>

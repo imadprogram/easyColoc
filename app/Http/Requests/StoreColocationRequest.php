35962f1c5d7f414279eb7cd75b3572f9e04bd1db
IMAD <kimad7141@gmail.com>
2026-03-02 00:22:13 +0000

    fixed something in colocation

--- app/Http/Requests/StoreColocationRequest.php
+++ app/Http/Requests/StoreColocationRequest.php
@@ -11,7 +11,7 @@ class StoreColocationRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return auth()->user() && auth()->user()->colocation_id === null;
+        return true;
     }
 
     /**
